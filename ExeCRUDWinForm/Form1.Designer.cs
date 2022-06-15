60535843fcbbd0379fd39276ebd783cf589718ee
Nur Sakinah <nursakinah2402@gmail.com>
2022-06-15 17:15:43 +0700

    Create Menu Form

--- ExeCRUDWinForm/Form1.Designer.cs
+++ ExeCRUDWinForm/Form1.Designer.cs
@@ -29,20 +29,92 @@ namespace ExeCRUDWinForm
         /// </summary>
         private void InitializeComponent()
         {
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button4
+            // 
+            this.button4.Image = global::ExeCRUDWinForm.Properties.Resources.delete_button;
+            this.button4.Location = new System.Drawing.Point(374, 322);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(199, 177);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Delete";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::ExeCRUDWinForm.Properties.Resources.system_update;
+            this.button3.Location = new System.Drawing.Point(126, 322);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(199, 177);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::ExeCRUDWinForm.Properties.Resources.search;
+            this.button2.Location = new System.Drawing.Point(374, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 177);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Search";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ExeCRUDWinForm.Properties.Resources.more2;
+            this.button1.Location = new System.Drawing.Point(126, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 177);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(188, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(335, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pet Shop Database";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(655, 471);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(719, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Menu";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
     }
 }
 
